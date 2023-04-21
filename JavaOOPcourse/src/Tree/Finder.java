1275426386c8bd13d946588153cf784172c1a339
MikhailNagornykh <stumpik@gmail.com>
2023-04-21 15:00:54 +0300

    Tree genealogy

---
+++ JavaOOPcourse/src/Tree/Finder.java
@@ -0,0 +1,10 @@
+package Tree;
+
+import java.util.List;
+
+public interface Finder {
+    public List getParents();
+    public List getChildren();
+    public List getSubParents();
+    public List getSubChildren();
+}
